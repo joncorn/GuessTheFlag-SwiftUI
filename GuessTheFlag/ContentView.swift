f21a219a362c9452484599ccda83ecdb8a0d38a4
Jon Corn <joncorn@Jons-MacBook-Pro.local>
2022-04-11 18:05:13 -0700

    add hi grid with v and hstacks

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // to add multiple text elements, they need to be contained in a stack
+        // Vstack meaning up-and-down orentation
+//        VStack(alignment: .leading) {
+//            Text("Hello, world!")
+//            Text("This is another text view")
+        // Created a grid using VStacks and HStacks
+        VStack {
+            HStack(spacing: 20) {
+                Text("hi")
+                Text("hi")
+                Text("hi")
+            }
+            HStack(spacing: 20) {
+                Text("hi")
+                Text("hi")
+                Text("hi")
+            }
+            HStack(spacing: 20) {
+                Text("hi")
+                Text("hi")
+                Text("hi")
+            }
+        }
     }
 }
 
