dfcb58013f0db6c963549cbe474df8df61e3e82e
Jon Corn <joncorn@Jons-MacBook-Pro.local>
2022-04-11 17:50:25 -0700

    add project files

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jon Corn on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
